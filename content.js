8711f8afdb979c0c021c748e4535e78ba60fe537
Yedidya Newlander <ydidya.n@gmail.com>
2023-07-26 00:18:42 +0300

    extension ready with side and promos deactivated

--- content.js
+++ content.js
@@ -5,43 +5,38 @@ function modifyPage() {
     if (element) {
         element.remove();
     }
-
     // Adjust the width in :root CSS
     let root = document.querySelector(':root');
     root.style.setProperty("--scaffold-layout-aside-width", "minmax(0,0px)", "important");
 
-    // Find all h2 elements
-    let headers = document.querySelectorAll('h2.feed-skip-link__container');
-    headers.forEach(header => {
-        // Get next sibling (the div after h2)
-        let nextDiv = header.nextElementSibling;
-        if (nextDiv && nextDiv.tagName.toLowerCase() === 'div') {
-            // Check if one of its children has the specified class
-            let hasClass = nextDiv.querySelector('.app-aware-link.update-components-actor__container-link.relative.display-flex.flex-grow-1');
-            if (hasClass) {
-                // Remove the h2
-                header.remove();
-                // Remove the div
-                nextDiv.remove();
-            }
+    // Find all div elements (assuming each post is in a div)
+    // Find all promoted posts
+    let promotedPosts = document.querySelectorAll('.feed-shared-update-v2');
+
+    promotedPosts.forEach((post) => {
+        let isPromoted = post.querySelector('.update-components-actor__sub-description');
+
+        if (isPromoted && isPromoted.textContent.includes('Promoted')) {
+            post.innerHTML = '<p>This post was a promotion and has been removed.</p>';
         }
     });
+
 }
 
-
 // Perform modifications if the extension is enabled
-chrome.storage.sync.get('toggleState', function(data) {
-    if (data.toggleState !== false) {
-        // Alert the user
-        alert('LinkedIn page cleanup activated.');
-
+chrome.storage.sync.get('toggleState', function (data) {
+    if (data.toggleState) {
         // Set delay (5000 milliseconds = 5 seconds)
         setTimeout(modifyPage, 5000);
 
-        // Create a MutationObserver instance to watch for changes in the document
-        let observer = new MutationObserver(modifyPage);
+        setInterval(modifyPage, 5000);
 
-        // Start observing the document with the configured parameters
-        observer.observe(document.body, { childList: true, subtree: true });
+        // // Create a MutationObserver instance to watch for changes in the document
+        // let observer = new MutationObserver(function(mutationsList, observer) {
+        //     modifyPage();
+        // });
+
+        // // Start observing the document with the configured parameters
+        // observer.observe(document.body, { childList: true, subtree: true });
     }
 });
--- popup.js
+++ popup.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const checkbox = document.getElementById('toggleVisibility');
     checkbox.addEventListener('change', function() {
+        // Save the state of the switch to chrome.storage
+        chrome.storage.sync.set({toggleState: this.checked}, function() {
+            console.log('Toggle state is set to ' + this.checked);
+        });
+
         // Reload the current tab
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.reload(tabs[0].id);
