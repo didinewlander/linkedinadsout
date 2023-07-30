5b89286f34d2add969863071255a0b019ebe6513
avishaiDV <avishaidv@gmail.com>
2023-07-30 21:20:38 +0300

    Removing the entire element instead of hiding it. Added an array of elements to remove intead of hard-coding it.

--- Source Code/content.js
+++ Source Code/content.js
@@ -1,48 +1,55 @@
 // Function to perform modifications
 function modifyPage() {
-    // Remove the <aside> element
-    let element = document.querySelector('aside');
-    if (element) {
-        element.style = "display:none";
-    }
-    element = document.querySelector('aside');
-    if (element) element.style = "display:none";
-    element = document.querySelector('footer');
-    if (element) element.style = "display:none";
-    // Adjust the width in :root CSS
-    let root = document.querySelector(':root');
-    root.style.setProperty("--scaffold-layout-aside-width", "minmax(0,0px)", "important");
+  //all elements to remove
+  const elementsNames = ["aside", "footer"];
 
-    // let responseBar = document.querySelector('feed-shared-social-actions')
-    // responseBar.style = "margin:0; justify-content: space-around";
-    // Find all div elements (assuming each post is in a div)
-    // Find all promoted posts
-    let promotedPosts = document.querySelectorAll('.feed-shared-update-v2');
+  elementsNames.forEach((name) => {
+    const element = document.querySelectorAll(name);
 
-    promotedPosts.forEach((post) => {
-        let isPromoted = post.querySelector('.update-components-actor__sub-description');
-
-        if (isPromoted && isPromoted.textContent.includes('Promoted')) {
-            post.style = "display:none";
-        }
+    element.forEach((e) => {
+      e.remove();
     });
+  });
 
+  // Adjust the width in :root CSS
+  let root = document.querySelector(":root");
+  root.style.setProperty(
+    "--scaffold-layout-aside-width",
+    "minmax(0,0px)",
+    "important"
+  );
+
+  // let responseBar = document.querySelector('feed-shared-social-actions')
+  // responseBar.style = "margin:0; justify-content: space-around";
+  // Find all div elements (assuming each post is in a div)
+  // Find all promoted posts
+  let promotedPosts = document.querySelectorAll(".feed-shared-update-v2");
+
+  promotedPosts.forEach((post) => {
+    let isPromoted = post.querySelector(
+      ".update-components-actor__sub-description"
+    );
+
+    if (isPromoted && isPromoted.textContent.includes("Promoted")) {
+      post.remove();
+    }
+  });
 }
 
 // Perform modifications if the extension is enabled
-chrome.storage.sync.get('toggleState', function (data) {
-    if (data.toggleState) {
-        // Set delay (5000 milliseconds = 5 seconds)
-        setTimeout(modifyPage, 5000);
+chrome.storage.sync.get("toggleState", function (data) {
+  if (data.toggleState) {
+    // Set delay (5000 milliseconds = 5 seconds)
+    setTimeout(modifyPage, 5000);
 
-        setInterval(modifyPage, 5000);
+    setInterval(modifyPage, 5000);
 
-        // // Create a MutationObserver instance to watch for changes in the document
-        // let observer = new MutationObserver(function(mutationsList, observer) {
-        //     modifyPage();
-        // });
+    // // Create a MutationObserver instance to watch for changes in the document
+    // let observer = new MutationObserver(function(mutationsList, observer) {
+    //     modifyPage();
+    // });
 
-        // // Start observing the document with the configured parameters
-        // observer.observe(document.body, { childList: true, subtree: true });
-    }
+    // // Start observing the document with the configured parameters
+    // observer.observe(document.body, { childList: true, subtree: true });
+  }
 });
