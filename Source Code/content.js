1ceadd24c619d8924663f16c9dfc9d72500fb96e
Yedidya Newlander <ydidya.n@gmail.com>
2023-10-02 12:26:13 +0300

    refactored modify page function to smaller funcs

--- Source Code/content.js
+++ Source Code/content.js
@@ -12,9 +12,9 @@ function toggleVisibility(element, isVisible) {
 function hidePromotedPosts() {
     let promotedPosts = document.querySelectorAll('.feed-shared-update-v2');
     promotedPosts.forEach((post) => {
-        let isPromoted = post.querySelector('.update-components-actor__sub-description');   
+        let isPromoted = post.querySelector('.update-components-actor__sub-description');
         if (isPromoted && isPromoted.textContent.includes('Promoted')) {
-            toggleVisibility(post, false); 
+            toggleVisibility(post, false);
         } else {
             toggleVisibility(post, true);
         }
@@ -22,15 +22,35 @@ function hidePromotedPosts() {
 }
 
 // Function to perform modifications
+function moveFooter() {
+    const footer = document.querySelector('footer');
+    const stickyContent = document.querySelector('.scaffold-layout__sticky-content');
+    if (footer && stickyContent) {
+        stickyContent.appendChild(footer);
+    } else {
+        console.error('Footer or sticky content element not found');
+    }
+}
+
+function setAsideWidth() {
+    const root = document.querySelector(':root');
+    if (root) {
+        root.style.setProperty("--scaffold-layout-aside-width", "minmax(0, 20px)", "important");
+    } else {
+        console.error('Root element not found');
+    }
+}
+
+function hidePromotedPosts() {
+    // Assuming this function is defined elsewhere in your code.
+}
+
 function modifyPage() {
-    toggleVisibility(document.querySelector('.ad-banner-container'), false); // Hide the promoted aside
-    let footer = document.querySelector('footer');
-    let sc = document.querySelector('.scaffold-layout__sticky-content');
-    if (footer && sc) sc.appendChild(footer);
-    toggleVisibility(document.querySelector('aside'), false);
-    let root = document.querySelector(':root');
-    if (root) root.style.setProperty("--scaffold-layout-aside-width", "minmax(0,20px)", "important");
-    hidePromotedPosts();
+    toggleVisibility(document.querySelector('.ad-banner-container'), false);  // Hide ad banner
+    moveFooter();  // Move footer inside sticky content
+    toggleVisibility(document.querySelector('aside'), false);  // Hide aside
+    setAsideWidth();  // Set aside width
+    hidePromotedPosts();  // Hide promoted posts
 }
 
 // Perform modifications if the extension is enabled
